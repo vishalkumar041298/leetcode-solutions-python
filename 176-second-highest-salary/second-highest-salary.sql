fc469dcb2784b72f64e95eef8fe1e23fab2c07d7
Vishal Kumar <50990822+vishalkumar041298@users.noreply.github.com>
2025-03-05 18:30:25 +0000

    Time: 679 ms (16.56%) | Memory: 0B (100.00%) - LeetSync

---
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -0,0 +1,6 @@
+WITH CTE AS
+			(SELECT Salary, dense_RANK () OVER (ORDER BY Salary desc) AS RANK_desc
+			   FROM Employee)
+SELECT MAX(salary) AS SecondHighestSalary
+  FROM CTE
+ WHERE RANK_desc = 2
