f2573528277f368ac65d04372368accfe5dfb143
Vishal Kumar <50990822+vishalkumar041298@users.noreply.github.com>
2025-06-05 23:27:32 +0100

    Time: 10 ms (75.15%) | Memory: 17.8 MB (86.15%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -5,12 +5,21 @@ class Solution:
         # return Counter(s) == Counter(t)
         if len(s) != len(t):
             return False
-        hashmap = {}
-        for i in s:
-            hashmap[i] = hashmap.get(i, 0) + 1
-        for r in t:
-            if r not in hashmap:
-                return False
-            hashmap[r] = hashmap.get(r) - 1
+        # hashmap = {}
+        # for i in s:
+        #     hashmap[i] = hashmap.get(i, 0) + 1
+        # for r in t:
+        #     if r not in hashmap:
+        #         return False
+        #     hashmap[r] = hashmap.get(r) - 1
         
-        return not any(hashmap.values())
+        # return not any(hashmap.values())
+
+        scount = [0] * 26
+        tcount = [0] * 26
+
+        for c in s:
+            scount[ord(c) - ord('a')] += 1
+        for c in t:
+            tcount[ord(c) - ord('a')] += 1
+        return scount == tcount
