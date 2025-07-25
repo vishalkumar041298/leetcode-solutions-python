311a57856c59c455f3ccd796d903b3819a99d48c
Vishal Kumar <50990822+vishalkumar041298@users.noreply.github.com>
2025-07-25 18:24:48 +0100

    Time: 11 ms (97.22%) | Memory: 18 MB (19.25%) - LeetSync

---
+++ 567-permutation-in-string/permutation-in-string.py
@@ -0,0 +1,28 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+
+        if len(s1) > len(s2):
+            return False
+        counts1 = [0] * 26
+        counts2 = [0] * 26
+
+
+        for i in range(len(s1)):
+            counts1[ord(s1[i])-ord('a')] += 1
+            counts2[ord(s2[i])-ord('a')] += 1
+        
+        if counts1 == counts2:
+            return True
+        l = 0
+        for r in range(len(s1), len(s2)):
+            counts2[ord(s2[r])-ord('a')] += 1
+            counts2[ord(s2[l])-ord('a')] -= 1
+
+
+            if counts1 == counts2:
+                return True
+            l += 1
+        return False
+
+
+            
