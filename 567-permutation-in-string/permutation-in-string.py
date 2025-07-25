c79283630fe543ccc11596b523906a85d20195eb
Vishal Kumar <50990822+vishalkumar041298@users.noreply.github.com>
2025-07-25 18:32:31 +0100

    Time: 17 ms (70.58%) | Memory: 18.1 MB (19.25%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -11,18 +11,33 @@ class Solution:
             counts1[ord(s1[i])-ord('a')] += 1
             counts2[ord(s2[i])-ord('a')] += 1
         
-        if counts1 == counts2:
-            return True
+        matches = 0
+        for i in range(26):
+            matches += 1 if counts1[i] == counts2[i] else 0 
+        
         l = 0
         for r in range(len(s1), len(s2)):
-            counts2[ord(s2[r])-ord('a')] += 1
-            counts2[ord(s2[l])-ord('a')] -= 1
-
-
-            if counts1 == counts2:
+            if matches == 26:
                 return True
+            
+            index = ord(s2[r]) - ord('a')
+            counts2[index] += 1
+
+            if counts1[index] == counts2[index]:
+                matches += 1
+            elif counts1[index] + 1 == counts2[index]:
+                matches -= 1
+            index = ord(s2[l]) - ord('a')
+
+
+            counts2[index] -= 1
+            if counts1[index] == counts2[index]:
+                matches += 1
+            elif counts1[index] - 1 == counts2[index]:
+                matches -= 1
+
             l += 1
-        return False
+        return matches == 26
 
 
             
